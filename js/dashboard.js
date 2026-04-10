ca6d78fa5d3402737738f14e9921818b247a81d1
Gabriel <Gabrielfc051ic@gmail.com>
2026-04-10 11:20:47 -0300

    TESTANDO O COMMIT

--- js/dashboard.js
+++ js/dashboard.js
@@ -81,3 +81,15 @@ function configurarMenuLateral() {
         sidebar.classList.add('collapsed');
     }
 }
+
+// segurança
+
+window.escapeHTML = function(str) {
+    if (str === null || str === undefined) return '';
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+};
